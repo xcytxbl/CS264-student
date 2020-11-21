10ca16e91f4a2de14ed76aa95d3cf5f7368aa166
xcytxbl <73339147+xcytxbl@users.noreply.github.com>
2020-11-22 02:32:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+app.get('/',function (req, res) {
+res.render('pages/index')
+});
+
+app.get('/branch', function (req, res,html) {
+ res.sendFile(path.join(__dirname+'/branch.html'));
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
