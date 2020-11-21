e26dea7b054b3055518b027c765f0abafe7989be
xcytxbl <73339147+xcytxbl@users.noreply.github.com>
2020-11-22 02:41:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/main_menu',function (req, res) {
 res.render('pages/main_menu.ejs')
 });
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
